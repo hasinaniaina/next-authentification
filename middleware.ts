ca1037145404f8f28fc42956708d6278dfb4f923
hasinaniaina <harilantorakotondrainibe@gmail.com>
2024-04-18 14:20:40 +0300

    Configuration middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,15 @@
-import { withAuth } from "next-auth/middleware"
-import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 
-export default withAuth({
-    secret: process.env.NEXTAUTH_SECRET
-})
-export const config = { matcher: "/home" }
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const session = !!req.cookies.get("next-auth.session-token")
+
+  if (!session) {
+    return NextResponse.redirect(new URL(`/api/auth/signin?callbackUrl=${path}`, req.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/home']
+}

08ff5149cb7efc2e565fea95f7e2c5d58408001f
hasinaniaina <harilantorakotondrainibe@gmail.com>
2024-04-18 13:38:43 +0300

    configuration middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,8 @@
-export { default } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware"
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(request: NextRequest) {
-    const token = await getToken({ req: request  });
-    const isAuthenticated = !!token;
-
-    if (isAuthenticated) {
-        return NextResponse.next();
-    }
-
-    return NextResponse.redirect(new URL("/", request.url));
-}
-
+export default withAuth({
+    secret: process.env.NEXTAUTH_SECRET
+})
 export const config = { matcher: "/home" }
